b091a101006f7278ce4679bf8672165605453b7d
Azay <azay.genius8@gmail.com>
2021-11-04 22:14:25 +0100

    Cartlist

---
+++ components/cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function CartList() {
+    return (
+        <View>
+            <Text>Cart </Text>
+        </View>
+    )
+}
+
